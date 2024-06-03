aeb32dd44bf13b545f19c83d8fdabe7bebf4cc05
Alexandre Le Corre <a.elreco@gmail.com>
2024-06-03 21:01:21 +0200

    update nav links

--- components/navbar/nav-links.tsx
+++ components/navbar/nav-links.tsx
@@ -1,54 +1,23 @@
 "use client";
 
 import clsx from "clsx";
-import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useRef, useState } from "react";
 
 import { links } from "@/utils/links";
 
 export function NavLinks() {
   const pathname = usePathname();
-  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-  const timeoutRef = useRef<number | null>(null);
 
-  return links.map(([label, href], index) => (
+  return links.map(([label, href]) => (
     <Link
       key={label}
       href={href}
       className={clsx(
-        pathname === href
-          ? "absolute inset-0 rounded-lg  bg-gray-900 text-gray-50 hover:text-gray-50"
-          : " text-gray-900 hover:text-gray-50",
-        "relative -mx-3 -my-2 rounded-lg px-3 py-2 text-sm transition-colors delay-150  hover:delay-0",
+        pathname === href && "bg-gray-900 !text-gray-50 hover:!bg-gray-700",
+        "relative -mx-3 -my-2 rounded-lg px-3 py-2 text-sm text-gray-900 transition-all duration-300 hover:text-gray-700",
       )}
-      onMouseEnter={() => {
-        if (timeoutRef.current) {
-          window.clearTimeout(timeoutRef.current);
-        }
-        setHoveredIndex(index);
-      }}
-      onMouseLeave={() => {
-        timeoutRef.current = window.setTimeout(() => {
-          setHoveredIndex(null);
-        }, 200);
-      }}
     >
-      <AnimatePresence>
-        {hoveredIndex === index && (
-          <motion.span
-            className="absolute inset-0 rounded-lg  bg-gray-900 text-gray-50"
-            layoutId="hoverBackground"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1, transition: { duration: 0.15 } }}
-            exit={{
-              opacity: 0,
-              transition: { duration: 0.15 },
-            }}
-          />
-        )}
-      </AnimatePresence>
       <span className="relative z-10">{label}</span>
     </Link>
   ));
