862e9fb522b608cdfe68f63efd47797a8df8cdc3
Alexandre Le Corre <a.elreco@gmail.com>
2024-06-08 21:57:57 +0200

    add iconoir

--- app/(default)/chats/[id]/page.tsx
+++ app/(default)/chats/[id]/page.tsx
@@ -168,6 +168,7 @@ export default function Chats({ params }: { params: { id: string } }) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
+  <link href="https://cdn.jsdelivr.net/gh/iconoir-icons/iconoir@main/css/iconoir.css" rel="stylesheet" />
   <link href="tailwindai.css" rel="stylesheet">
 </head>
 ${completion}
