623c2ab45e7f870273d12b0c4e81ba497737e02d
Alexandre Le Corre <a.elreco@gmail.com>
2024-06-16 16:06:31 +0200

    update pricing

--- app/(default)/pricing/Pricing.tsx
+++ app/(default)/pricing/Pricing.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { CheckIcon } from "@heroicons/react/24/solid";
 import { User } from "@supabase/supabase-js";
 import clsx from "clsx";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
+import { Container } from "@/components/container";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/components/ui/toaster/use-toast";
 import { Database } from "@/types_db";
@@ -106,82 +108,93 @@ export default function Pricing({ user, products, subscription }: Props) {
 
   if (products.length === 1)
     return (
-      <section className="min-h-screen">
-        <div className="mx-auto max-w-6xl px-4 pt-24 sm:px-6 lg:px-8">
-          <div className="sm:flex sm:flex-col sm:items-center">
-            <h1 className="text-xl font-bold text-gray-900 sm:text-center sm:text-4xl">
-              Pricing Plans
-            </h1>
-            <p className="m-auto mt-5 max-w-2xl text-lg text-gray-700 sm:text-center sm:text-xl">
-              Start building for free, then add a site plan to go live. Account
-              plans unlock additional features.
-            </p>
-            <div className="relative mt-12 flex self-center rounded-lg">
-              <div className="divide-y rounded-lg border bg-zinc-900 shadow-sm">
-                <div className="m-1 whitespace-nowrap rounded-md border-zinc-800 p-6 py-2 text-2xl font-medium text-white shadow-sm focus:z-10 focus:outline-none focus:ring-2 focus:ring-opacity-50 sm:w-auto sm:px-8">
-                  {products[0].name}
-                </div>
-              </div>
-            </div>
-            <div className="mt-6 space-y-4 sm:mt-12 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:mx-auto lg:max-w-4xl xl:mx-0 xl:max-w-none xl:grid-cols-3">
-              {products[0].prices?.map((price) => {
-                const priceString =
-                  price.unit_amount &&
-                  new Intl.NumberFormat("en-US", {
-                    style: "currency",
-                    currency: price.currency!,
-                    minimumFractionDigits: 0,
-                  }).format(price.unit_amount / 100);
-
-                return (
-                  <div
-                    key={price.interval}
-                    className="divide-y divide-zinc-600 rounded-lg border bg-white"
-                  >
-                    <div className="p-6">
-                      <p>
-                        <span className="white text-5xl font-bold">
-                          {priceString}
-                        </span>
-                        <span className="text-base font-medium text-gray-700">
-                          /{price.interval}
-                        </span>
-                      </p>
-                      <p className="mt-4 text-gray-700">{price.description}</p>
-                      <Button
-                        variant="solid"
-                        type="button"
-                        disabled={false}
-                        loading={priceIdLoading === price.id}
-                        onClick={() => handleCheckout(price)}
-                        className="mt-12 block w-full rounded-md py-2 text-center text-sm font-medium text-white hover:bg-gray-900 "
-                      >
-                        {products[0].name ===
-                        subscription?.prices?.products?.name
-                          ? "Manage"
-                          : "Subscribe"}
-                      </Button>
-                    </div>
-                  </div>
-                );
-              })}
+      <Container>
+        <h1 className="mb-1 text-lg font-medium text-gray-900 sm:text-left sm:text-2xl">
+          Pricing Plans
+        </h1>
+        <h2 className="mb-8 text-lg text-gray-700 sm:text-left sm:text-xl">
+          Start building for free, then add a site plan to go live. Account
+          plans unlock additional features.
+        </h2>
+        {/* <div className="relative mt-12 flex self-center rounded-lg">
+          <div className="divide-y rounded-lg border bg-zinc-900 shadow-sm">
+            <div className="m-1 whitespace-nowrap rounded-md border-zinc-800 p-6 py-2 text-2xl font-medium text-white shadow-sm focus:z-10 focus:outline-none focus:ring-2 focus:ring-opacity-50 sm:w-auto sm:px-8">
+              {products[0].name}
             </div>
           </div>
+        </div> */}
+        <div className="mt-6 h-2/3 items-center space-y-4 sm:mt-12 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:mx-auto lg:max-w-4xl xl:mx-0 xl:max-w-none xl:grid-cols-3">
+          {products[0].prices?.map((price) => {
+            const priceString =
+              price.unit_amount &&
+              new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: price.currency!,
+                minimumFractionDigits: 0,
+              }).format(price.unit_amount / 100);
+
+            return (
+              <div
+                key={price.interval}
+                className="divide-y divide-zinc-600 rounded-lg border bg-white"
+              >
+                <div className="p-6">
+                  <p>
+                    <span className="white text-5xl font-bold">
+                      {priceString}
+                    </span>
+                    <span className="text-base font-medium text-gray-700">
+                      /{price.interval}
+                    </span>
+                  </p>
+                  <p className="mt-4 text-gray-700">{price.description}</p>
+                  <p className="mt-4 flex items-center text-sm font-medium text-gray-700">
+                    <CheckIcon className="mr-2 size-4 text-emerald-500" />{" "}
+                    Unlinimited credits
+                  </p>
+                  <p className="mt-4 flex items-center text-sm font-medium text-gray-700">
+                    <CheckIcon className="mr-2 size-4 text-emerald-500" />{" "}
+                    Unlinimited versions
+                  </p>
+                  <p className="mt-4 flex items-center text-sm font-medium text-gray-700">
+                    <CheckIcon className="mr-2 size-4 text-emerald-500" />{" "}
+                    Unlinimited components with image
+                  </p>
+                  <p className="mt-4 flex items-center text-sm font-medium text-gray-700">
+                    <CheckIcon className="mr-2 size-4 text-emerald-500" /> Fast
+                    generation
+                  </p>
+                  <Button
+                    variant="solid"
+                    type="button"
+                    disabled={false}
+                    loading={priceIdLoading === price.id}
+                    onClick={() => handleCheckout(price)}
+                    className="mt-12 block w-full rounded-md py-2 text-center text-sm font-medium text-white hover:bg-gray-900 "
+                  >
+                    {products[0].name === subscription?.prices?.products?.name
+                      ? "Manage"
+                      : "Subscribe"}
+                  </Button>
+                </div>
+              </div>
+            );
+          })}
         </div>
-      </section>
+      </Container>
     );
 
   return (
     <section className="bg-black">
       <div className="mx-auto max-w-6xl px-4 py-8 sm:px-6 sm:py-24 lg:px-8">
         <div className="sm:flex sm:flex-col sm:items-center">
-          <h1 className="text-xl font-semibold text-white sm:text-center sm:text-6xl">
-            Pricing Plans
+          <h1 className="mb-1 text-lg font-medium text-gray-900 sm:text-left sm:text-2xl">
+            Pricing Plans 2
           </h1>
-          <p className="m-auto mt-5 max-w-2xl text-xl text-zinc-200 sm:text-center sm:text-2xl">
+          <h2 className="mb-8 text-lg text-gray-700 sm:text-left sm:text-xl">
             Start building for free, then add a site plan to go live. Account
             plans unlock additional features.
-          </p>
+          </h2>
           <div className="relative mt-6 flex self-center rounded-lg border border-zinc-800 bg-zinc-900 p-0.5 sm:mt-8">
             {intervals.includes("month") && (
               <button
