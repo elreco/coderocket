bee91a302b389921558b29f9cf58d6a34460e196
Alexandre Le Corre <a.elreco@gmail.com>
2024-06-17 11:03:45 +0200

    update content

--- app/(content)/content/[id]/layout.tsx
+++ app/(content)/content/[id]/layout.tsx
@@ -1,10 +1,9 @@
 /* eslint-disable import/order */
 import { PropsWithChildren } from "react";
 
-import "styles/content.css";
-import "styles/iconoir.css";
 import { Inter } from "next/font/google";
 import clsx from "clsx";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -14,8 +13,21 @@ const inter = Inter({
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html lang="en" className={clsx("size-full antialiased", inter.variable)}>
-      {children}
+    <html className={clsx("size-full antialiased", inter.variable)}>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Tailwind AI</title>
+        <Script
+          strategy="beforeInteractive"
+          src="https://cdn.tailwindcss.com"
+        />
+        <link
+          href="https://cdn.jsdelivr.net/gh/iconoir-icons/iconoir@main/css/iconoir.css"
+          rel="stylesheet"
+        />
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
