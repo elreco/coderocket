97db0ed2aa3612aca32d845b5c474ed3792e3ab8
Alexandre Le Corre <a.elreco@gmail.com>
2024-06-20 12:08:15 +0200

    update capture website

--- utils/capture-screenshot.ts
+++ utils/capture-screenshot.ts
@@ -1,8 +1,16 @@
 import captureWebsite from "capture-website";
+import chromium from "chrome-aws-lambda";
 
 export async function captureScreenshot(url: string) {
   try {
-    const response = await captureWebsite.buffer(url);
+    const response = await captureWebsite.buffer(url, {
+      launchOptions: {
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+      },
+    });
     return response;
   } catch (error) {
     console.error("Error taking screenshot:", error);
