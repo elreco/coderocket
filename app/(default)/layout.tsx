8187a1714a0d2dcaebd979431cebb7e30db585cb
Alexandre Le Corre <a.elreco@gmail.com>
2024-08-19 11:35:59 +0200

    add brevo

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -71,6 +71,22 @@ export default function RootLayout({ children }: PropsWithChildren) {
     >
       <head>
         <SandPackCSS />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(d, w, c) {
+                  w.BrevoConversationsID = '66c30f09e1078bbc630253d8';
+                  w[c] = w[c] || function() {
+                      (w[c].q = w[c].q || []).push(arguments);
+                  };
+                  var s = d.createElement('script');
+                  s.async = true;
+                  s.src = 'https://conversations-widget.brevo.com/brevo-conversations.js';
+                  if (d.head) d.head.appendChild(s);
+              })(document, window, 'BrevoConversations');
+            `,
+          }}
+        ></script>
       </head>
       <TooltipProvider>
         <body className="size-full">
---
+++ global.d.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// global.d.ts
+declare global {
+  interface Window {
+    BrevoConversationsID: string;
+    BrevoConversations: any;
+  }
+}
