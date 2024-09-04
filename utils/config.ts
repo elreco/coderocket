63aa452e4ec37f751403f7b93358e31722ce02e2
Alexandre Le Corre <a.elreco@gmail.com>
2024-09-04 22:18:55 +0200

    add placeholder

--- utils/config.ts
+++ utils/config.ts
@@ -7,3 +7,14 @@ export const screenshotApiUrl =
 export const maxPromptLength = 1000;
 
 export const openAIModel = "gpt-4o-mini";
+
+export const getHtmlContent = (completion: string) => `<html class="size-full">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="https://cdn.tailwindcss.com"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<link href="tailwindai.css" rel="stylesheet">
+</head>
+${completion}
+</html>`;
