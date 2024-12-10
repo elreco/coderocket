c304d3ba0a43617ae603a59da57b7f91f39c9d1b
Alexandre Le Corre <alexandrelecorre.pro@gmail.com>
2024-12-10 17:30:28 +0100

    add font awesome cdn

--- utils/iframe-builder.ts
+++ utils/iframe-builder.ts
@@ -16,6 +16,7 @@ https://tailwindai.dev/components/${id}
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.19/dist/full.min.css" rel="stylesheet" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   </head>
   ${completion}
 </html>`;
