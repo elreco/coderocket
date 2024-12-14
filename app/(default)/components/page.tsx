4802a97cf77810c165ce00158b33ab52cb731e0f
Alexandre Le Corre <alexandrelecorre.pro@gmail.com>
2024-12-14 02:55:34 +0100

    no console

--- app/(default)/components/page.tsx
+++ app/(default)/components/page.tsx
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default async function Featured() {
   const chats = await getAllPublicChats();
-  console.log("chats", chats);
+
   return (
     <Container>
       <PageTitle
