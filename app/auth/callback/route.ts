33170f27391d1ad53b80ded774d1f47c51af9537
Alexandre Le Corre <alexandrelecorre.pro@gmail.com>
2024-12-19 21:34:58 +0100

    update revalidate

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,3 +1,4 @@
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 // The client you created from the Server-Side Auth instructions
@@ -13,20 +14,23 @@ export async function GET(request: Request) {
     const supabase = await createClient();
     const { error, data } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      if (data.user?.user_metadata.full_name) {
+      if (data.user?.user_metadata?.full_name && data.user?.id) {
         await supabase
           .from("users")
-          .update({ full_name: data.user?.user_metadata.full_name })
-          .eq("id", data.user?.id);
+          .update({ full_name: data.user.user_metadata.full_name })
+          .eq("id", data.user.id);
       }
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
       if (isLocalEnv) {
+        revalidatePath(`${origin}${next}`);
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
         return NextResponse.redirect(`${origin}${next}`);
       } else if (forwardedHost) {
+        revalidatePath(`https://${forwardedHost}${next}`);
         return NextResponse.redirect(`https://${forwardedHost}${next}`);
       } else {
+        revalidatePath(`${origin}${next}`);
         return NextResponse.redirect(`${origin}${next}`);
       }
     }
