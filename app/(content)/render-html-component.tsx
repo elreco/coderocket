d5bd1bf39f3242543ef5850b644b10f97ee3dfd4
Alexandre Le Corre <alexandrelecorre.pro@gmail.com>
2024-12-28 21:50:48 +0100

    update

--- app/(content)/render-html-component.tsx
+++ app/(content)/render-html-component.tsx
@@ -15,59 +15,59 @@ export default function RenderHtmlComponent({
     setContent(files[0]?.content || "");
   }, [files]);
 
-  // useEffect(() => {
-  //   const iframe = document.querySelector("iframe");
-  //   if (!iframe) {
-  //     console.warn("Iframe non trouvé");
-  //     return;
-  //   }
+  useEffect(() => {
+    const iframe = document.querySelector("iframe");
+    if (!iframe) {
+      console.warn("Iframe non trouvé");
+      return;
+    }
 
-  //   const handleLinkClick = (event: MouseEvent) => {
-  //     const target = event.target as HTMLAnchorElement;
-  //     const href = target.getAttribute("href");
+    const handleLinkClick = (event: MouseEvent) => {
+      const target = event.target as HTMLAnchorElement;
+      const href = target.getAttribute("href");
 
-  //     if (href?.startsWith("#")) {
-  //       event.preventDefault();
-  //       const anchorId = href.substring(1);
-  //       const iframeDocument =
-  //         iframe.contentDocument || iframe.contentWindow?.document;
-  //       const anchorElement = iframeDocument?.getElementById(anchorId);
-  //       if (anchorElement) {
-  //         anchorElement.scrollIntoView({ behavior: "smooth" });
-  //       }
-  //       return;
-  //     }
+      if (href?.startsWith("#")) {
+        event.preventDefault();
+        const anchorId = href.substring(1);
+        const iframeDocument =
+          iframe.contentDocument || iframe.contentWindow?.document;
+        const anchorElement = iframeDocument?.getElementById(anchorId);
+        if (anchorElement) {
+          anchorElement.scrollIntoView({ behavior: "smooth" });
+        }
+        return;
+      }
 
-  //     event.preventDefault();
-  //     const fileName = href?.replace("./", "");
-  //     if (fileName) {
-  //       const fileContent = files.find((file) => file.name === fileName);
-  //       setContent(fileContent?.content || "");
-  //     } else {
-  //       console.warn("Aucun nom de fichier trouvé dans l'attribut href.");
-  //     }
-  //   };
+      event.preventDefault();
+      const fileName = href?.replace("./", "");
+      if (fileName) {
+        const fileContent = files.find((file) => file.name === fileName);
+        setContent(fileContent?.content || "");
+      } else {
+        console.warn("Aucun nom de fichier trouvé dans l'attribut href.");
+      }
+    };
 
-  //   const onLoad = () => {
-  //     const iframeDocument =
-  //       iframe.contentDocument || iframe.contentWindow?.document;
-  //     if (!iframeDocument) {
-  //       console.warn("Document de l'iframe non accessible");
-  //       return;
-  //     }
+    const onLoad = () => {
+      const iframeDocument =
+        iframe.contentDocument || iframe.contentWindow?.document;
+      if (!iframeDocument) {
+        console.warn("Document de l'iframe non accessible");
+        return;
+      }
 
-  //     const links = iframeDocument.querySelectorAll("a");
-  //     links.forEach((link) => {
-  //       link.addEventListener("click", handleLinkClick);
-  //     }); // Délai de 100ms
-  //   };
+      const links = iframeDocument.querySelectorAll("a");
+      links.forEach((link) => {
+        link.addEventListener("click", handleLinkClick);
+      }); // Délai de 100ms
+    };
 
-  //   iframe.addEventListener("load", onLoad);
+    iframe.addEventListener("load", onLoad);
 
-  //   return () => {
-  //     iframe.removeEventListener("load", onLoad);
-  //   };
-  // }, [files]);
+    return () => {
+      iframe.removeEventListener("load", onLoad);
+    };
+  }, [files]);
 
   return (
     <iframe
