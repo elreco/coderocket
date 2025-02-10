ecb2fe7423ea35d746fd6776265a41089cf9cd2c
Alexandre LE CORRE <alexandrelecorre.pro@gmail.com>
2025-02-10 19:51:20 +0100

    update pricing

--- app/(default)/pricing/pricing.tsx
+++ app/(default)/pricing/pricing.tsx
@@ -94,9 +94,7 @@ export default function Pricing({ user, products, subscription }: Props) {
     }).format(price / 100);
   };
 
-  if (products.length === 1) {
-    const prices = products[0].prices || [];
-
+  if (products.length >= 1) {
     return (
       <div className="flex items-center">
         <div className="my-4 size-full items-center space-y-4 sm:my-4 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:mx-auto lg:max-w-4xl xl:mx-0 xl:max-w-none xl:grid-cols-4 xl:space-y-0">
@@ -149,117 +147,124 @@ export default function Pricing({ user, products, subscription }: Props) {
           </div>
 
           {/* Plans payants */}
-          {prices.map((price) => {
-            const priceString = formatPrice(
-              price.unit_amount || 0,
-              price.currency!,
-            );
-
-            /* // Calculer l'économie par rapport au plan Starter
-            const starterPrice = 180 * 100; // Convert to cents
-            const savings =
-              price.description !== "Starter Plan"
-                ? calculateSavings(price.unit_amount || 0, starterPrice)
-                : null; */
-
-            // Description personnalisée
-            const planDescription =
-              price.description === "Starter Plan" ? (
-                <p className="mt-4 ">
-                  <span className="font-bold">Perfect for starters!</span>{" "}
-                  Generate with Image included.
-                </p>
-              ) : price.description === "Pro Plan" ? (
-                <p className="mt-4 ">
-                  <span className="font-bold">Go Pro!</span> Unlock advanced
-                  features like AI Full Power for seamless workflows.
-                </p>
-              ) : (
-                <p className="mt-4 ">
-                  <span className="font-bold">❤️ Support Tailwind AI</span>
-                  &apos;s development while enjoying the best value!
-                </p>
+          {products
+            .flatMap((product) =>
+              product.prices.map((price) => ({ product, price })),
+            )
+            .sort(
+              (a, b) => (a.price.unit_amount || 0) - (b.price.unit_amount || 0),
+            )
+            .map(({ product, price }) => {
+              const priceString = formatPrice(
+                price.unit_amount || 0,
+                price.currency!,
               );
 
-            return (
-              <div
-                key={price.id}
-                className="flex h-full flex-col divide-y divide-border rounded-lg border bg-card p-3"
-              >
-                <h3 className="mb-4 pl-3 text-lg font-bold text-white">
-                  {price.description}
-                </h3>
-                <div className="grow p-3">
-                  <p>
-                    <span className="text-5xl font-bold text-primary">
-                      {priceString}
-                    </span>
-                    <span className="text-base font-medium text-white">
-                      /{price.interval}
-                    </span>
-                  </p>
-                  {planDescription}
-                  <p className="mt-4 flex items-center text-sm font-medium ">
-                    <Check className="mr-2 size-4 text-emerald-500" /> Generate
-                    with Image
-                  </p>
-                  <p className="mt-4 flex items-center text-sm font-medium ">
-                    <Check className="mr-2 size-4 text-emerald-500" /> Improve
-                    prompt
-                  </p>
-                  <p className="mt-4 flex items-center text-sm font-medium ">
-                    <Check className="mr-2 size-4 text-emerald-500" />
-                    {price.description === "Starter Plan"
-                      ? STARTER_PLAN_MESSAGES_PER_PERIOD
-                      : price.description === "Pro Plan"
-                        ? PRO_PLAN_MESSAGES_PER_PERIOD
-                        : SUPPORTER_PLAN_MESSAGES_PER_PERIOD}{" "}
-                    messages per month
-                  </p>
+              /* // Calculer l'économie par rapport au plan Starter
+              const starterPrice = 180 * 100; // Convert to cents
+              const savings =
+                price.description !== "Starter Plan"
+                  ? calculateSavings(price.unit_amount || 0, starterPrice)
+                  : null; */
 
-                  {price.description === "Starter Plan" ? (
-                    <>
-                      <p className="mt-4 flex items-center text-sm font-medium ">
-                        <Check className="mr-2 size-4 text-emerald-500" />{" "}
-                        Support
-                      </p>
-                      <p className="mt-4 flex items-center text-sm font-medium ">
-                        <XIcon className="mr-2 size-4 text-border" /> AI Full
-                        Power
-                      </p>
-                      <p className="mt-4 flex items-center text-sm font-medium ">
-                        <XIcon className="mr-2 size-4 text-border" /> Extended
-                        support
-                      </p>
-                    </>
-                  ) : (
-                    <>
-                      <p className="mt-4 flex items-center text-sm font-medium ">
-                        <Check className="mr-2 size-4 text-emerald-500" />{" "}
-                        Extended support
-                      </p>
-                      <p className="mt-4 flex items-center text-sm font-medium ">
-                        <Check className="mr-2 size-4 text-emerald-500" /> AI
-                        Full Power
-                      </p>
-                    </>
-                  )}
-                </div>
-                <Button
-                  variant="default"
-                  type="button"
-                  disabled={false}
-                  loading={priceIdLoading === price.id}
-                  onClick={() => handleCheckout(price)}
-                  className="block w-full rounded-full text-white"
+              // Description personnalisée basée sur le nom du produit
+              const planDescription =
+                product.name === "Starter Plan" ? (
+                  <p className="mt-4 ">
+                    <span className="font-bold">Perfect for starters!</span>{" "}
+                    Generate with Image included.
+                  </p>
+                ) : product.name === "Pro Plan" ? (
+                  <p className="mt-4 ">
+                    <span className="font-bold">Go Pro!</span> Unlock advanced
+                    features like AI Full Power for seamless workflows.
+                  </p>
+                ) : (
+                  <p className="mt-4 ">
+                    <span className="font-bold">❤️ Support Tailwind AI</span>
+                    &apos;s development while enjoying the best value!
+                  </p>
+                );
+
+              return (
+                <div
+                  key={price.id}
+                  className="flex h-full flex-col divide-y divide-border rounded-lg border bg-card p-3"
                 >
-                  {products[0].name === subscription?.prices?.products?.name
-                    ? "Manage"
-                    : "Subscribe"}
-                </Button>
-              </div>
-            );
-          })}
+                  <h3 className="mb-4 pl-3 text-lg font-bold text-white">
+                    {product.name}
+                  </h3>
+                  <div className="grow p-3">
+                    <p>
+                      <span className="text-5xl font-bold text-primary">
+                        {priceString}
+                      </span>
+                      <span className="text-base font-medium text-white">
+                        /{price.interval}
+                      </span>
+                    </p>
+                    {planDescription}
+                    <p className="mt-4 flex items-center text-sm font-medium ">
+                      <Check className="mr-2 size-4 text-emerald-500" />{" "}
+                      Generate with Image
+                    </p>
+                    <p className="mt-4 flex items-center text-sm font-medium ">
+                      <Check className="mr-2 size-4 text-emerald-500" /> Improve
+                      prompt
+                    </p>
+                    <p className="mt-4 flex items-center text-sm font-medium ">
+                      <Check className="mr-2 size-4 text-emerald-500" />
+                      {product.name === "Starter Plan"
+                        ? STARTER_PLAN_MESSAGES_PER_PERIOD
+                        : product.name === "Pro Plan"
+                          ? PRO_PLAN_MESSAGES_PER_PERIOD
+                          : SUPPORTER_PLAN_MESSAGES_PER_PERIOD}{" "}
+                      messages per month
+                    </p>
+
+                    {product.name === "Starter" ? (
+                      <>
+                        <p className="mt-4 flex items-center text-sm font-medium ">
+                          <Check className="mr-2 size-4 text-emerald-500" />{" "}
+                          Support
+                        </p>
+                        <p className="mt-4 flex items-center text-sm font-medium ">
+                          <XIcon className="mr-2 size-4 text-border" /> AI Full
+                          Power
+                        </p>
+                        <p className="mt-4 flex items-center text-sm font-medium ">
+                          <XIcon className="mr-2 size-4 text-border" /> Extended
+                          support
+                        </p>
+                      </>
+                    ) : (
+                      <>
+                        <p className="mt-4 flex items-center text-sm font-medium ">
+                          <Check className="mr-2 size-4 text-emerald-500" />{" "}
+                          Extended support
+                        </p>
+                        <p className="mt-4 flex items-center text-sm font-medium ">
+                          <Check className="mr-2 size-4 text-emerald-500" /> AI
+                          Full Power
+                        </p>
+                      </>
+                    )}
+                  </div>
+                  <Button
+                    variant="default"
+                    type="button"
+                    disabled={false}
+                    loading={priceIdLoading === price.id}
+                    onClick={() => handleCheckout(price)}
+                    className="block w-full rounded-full text-white"
+                  >
+                    {product.name === subscription?.prices?.products?.name
+                      ? "Manage"
+                      : "Subscribe"}
+                  </Button>
+                </div>
+              );
+            })}
         </div>
       </div>
     );
