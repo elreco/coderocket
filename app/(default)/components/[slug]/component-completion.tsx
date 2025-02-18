7f84366c5cc9dcafcb45788f522bc503ad060d6b
Alexandre LE CORRE <alexandrelecorre.pro@gmail.com>
2025-02-18 14:41:56 +0100

    update z index

--- app/(default)/components/[slug]/component-completion.tsx
+++ app/(default)/components/[slug]/component-completion.tsx
@@ -646,7 +646,7 @@ export default function ComponentCompletion({
             </div>
             <div className="relative m-0 flex h-full max-h-full flex-1 flex-col border-b lg:border-b-0">
               {!isLoading && (
-                <Badge className="absolute bottom-0 right-0 z-[100] m-2 hover:bg-primary">
+                <Badge className="absolute bottom-0 right-0 z-[49] m-2 hover:bg-primary">
                   <FrameworkIcon className="mr-1 size-3" />
                   <span className="first-letter:uppercase">
                     {fetchedChat?.framework}
