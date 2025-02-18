d3ec37445c45d61028fe6df0b07b3e264b8c153d
Alexandre LE CORRE <alexandrelecorre.pro@gmail.com>
2025-02-18 14:45:52 +0100

    fix

--- app/(content)/preview/[prefix]/[[...slug]]/page.tsx
+++ app/(content)/preview/[prefix]/[[...slug]]/page.tsx
@@ -1,59 +1,19 @@
-import { Metadata, ResolvingMetadata } from "next";
-
-import {
-  fetchAssistantMessageByChatIdAndVersion,
-  fetchFirstUserMessageByChatId,
-  fetchChatById,
-} from "@/app/(default)/components/actions";
 import { Watermark } from "@/components/watermark";
-import { capitalizeFirstLetter } from "@/utils/helpers";
 
-interface Props {
+export default async function Page({
+  params,
+  searchParams,
+}: {
   params: Promise<{ prefix: string; slug?: string[] }>;
   searchParams: Promise<{ watermark?: string }>;
-}
-
-export async function generateMetadata(
-  { params }: Props,
-  parent: ResolvingMetadata,
-): Promise<Metadata> {
-  const { prefix } = await params;
-  const parts = prefix.split("-");
-  const versionNumber = Number(parts.pop());
-  const id = parts.join("-");
-
-  const lastAssistantMessage = await fetchAssistantMessageByChatIdAndVersion(
-    id,
-    versionNumber,
-  );
-
-  const firstUserMessage = await fetchFirstUserMessageByChatId(id);
-
-  // optionally access and extend (rather than replace) parent metadata
-  const previousImages = (await parent).openGraph?.images || [];
-
-  return {
-    title: `${capitalizeFirstLetter(
-      firstUserMessage?.content?.toString() || "",
-      15,
-    )} - Tailwind AI`,
-    openGraph: {
-      images: lastAssistantMessage?.screenshot
-        ? [lastAssistantMessage.screenshot]
-        : [...previousImages],
-    },
-  };
-}
-
-export default async function Page({ params }: Props) {
+}) {
   const { prefix, slug } = await params;
-  const parts = prefix.split("-");
-  const id = parts.join("-");
-  const chat = await fetchChatById(id);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { watermark } = await searchParams;
 
   return (
     <div className="size-full">
-      {chat.slug && <Watermark slug={chat.slug} />}
+      <Watermark />
       <iframe
         className="size-full border-none"
         src={`https://${prefix}.webcontainer.tailwindai.dev/${slug ? slug.join("/") : ""}`}
