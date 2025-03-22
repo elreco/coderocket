d856804f2f264fc714c9d223a4d17376ce2c2437
Alexandre LE CORRE <alexandrelecorre.pro@gmail.com>
2025-03-22 02:50:08 +0100

    update pricing

--- utils/config.ts
+++ utils/config.ts
@@ -93,8 +93,8 @@ export const builderApiUrl = "https://react-builder.fly.dev";
 export const avatarApi =
   "https://api.dicebear.com/9.x/avataaars-neutral/svg?seed=";
 
-export const STARTER_PLAN_MESSAGES_PER_PERIOD = 200;
-export const PRO_PLAN_MESSAGES_PER_PERIOD = 500;
+export const STARTER_PLAN_MESSAGES_PER_PERIOD = 100;
+export const PRO_PLAN_MESSAGES_PER_PERIOD = 300;
 export const DEFAULT_MESSAGES_PER_PERIOD = 100;
 export const TRIAL_PLAN_MESSAGES_PER_MONTH = 6;
 
