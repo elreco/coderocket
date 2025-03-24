34069f5340ba21720122e542405e070185f46eba
Alexandre LE CORRE <alexandrelecorre.pro@gmail.com>
2025-03-24 09:14:57 +0100

    40

--- utils/config.ts
+++ utils/config.ts
@@ -107,7 +107,7 @@ export const MAX_ACCOUNTS_PER_IP = 1;
 export const FREE_CHAR_LIMIT = 1500;
 export const PREMIUM_CHAR_LIMIT = MAX_TOKENS_PER_REQUEST * CHAR_PER_TOKEN; // 12000 caractères
 
-export const MAX_VERSIONS_PER_COMPONENT = 50;
+export const MAX_VERSIONS_PER_COMPONENT = 40;
 
 export const getCharacterLimit = (
   subscription:
