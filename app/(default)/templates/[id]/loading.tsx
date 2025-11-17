d426a59a5e7048d4bb258fd2b38b952a43c9f81f
Alexandre LE CORRE <alexandrelecorre.pro@gmail.com>
2025-11-17 20:37:29 +0100

    update loading

--- app/(default)/templates/[id]/loading.tsx
+++ app/(default)/templates/[id]/loading.tsx
@@ -4,8 +4,8 @@ import Logo from "@/components/icons/logo";
 export default function Loading() {
   return (
     <Container>
-      <div className="relative flex size-full animate-pulse items-center justify-center rounded-md bg-background ">
-        <Logo className="w-16" />
+      <div className="relative flex size-full items-center justify-center rounded-md bg-background ">
+        <Logo className="w-16 animate-pulse" />
       </div>
     </Container>
   );
